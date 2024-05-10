4690f3f951eb680efb6bce6f9af46c28d39dca3f
MAVERICK-VF142 <ashutosh.jena.mav.1@gmail.com>
2024-05-10 17:11:39 +0530

    cleaned the tree

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-            current_perspective = "back"
-    else:
-        # Reset video capture to start from the beginning
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
-# Release the video capture object and close the d
