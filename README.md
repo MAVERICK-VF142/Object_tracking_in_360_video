e62fe58c011920f20f5aeb461a4acdc74cf1c092
AverageBlank <hussainkhorakiwala45@gmail.com>
2024-10-17 16:17:36 +0530

    Changes to .gitignore, requirements.txt, README.md, app.py - .gitignore: Added exclusions for .venv, __pycache__ and   boundary_tracking.log - requirements.txt: Changed numpy version from 1.24.4 to 1.26.4 as there   was a building error in the previous version. Also added a few   libraries being: torchreid, scikit-learn, scikit-image, ultralytics,   tensorboard - README.md: Added additional requirements that were added to the requirements.txt   file. Also formatted the file using prettier. - app.py: Fixed an instance where a function was defined twice, also   formatted the file using black.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# 🤖 YOLOv10 Object Tracking 
+# 🤖 YOLOv10 Object Tracking
 
 <div style="text-align: center;">
     <img src="https://github.com/user-attachments/assets/ad6c0817-c112-4936-86a9-578190e5fd89" alt="welcome-gif-24" width="300" />
 </div>
 
 ## 🌍 Get Ready to Track Everything! Step into the YOLOv10 Object Tracking Universe! 🌌
+
 <div style="display: flex; align-items: center;">
     <img src="https://img.shields.io/github/stars/MAVERICK-VF142/Object_tracking_in_360_video" alt="Repo Stars" width="120" style="margin-right: 10px;" />
     <img src="https://img.shields.io/github/forks/MAVERICK-VF142/Object_tracking_in_360_video" alt="Forks" width="120" style="margin-right: 10px;" />
@@ -26,10 +27,9 @@ This repository contains a Python script for real-time object tracking using YOL
 
 <img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="900">
 
-
 # 🚀 Features
-## 🔍 Dive into Our Features! Unleash the Power of This Project! 💥
 
+## 🔍 Dive into Our Features! Unleash the Power of This Project! 💥
 
 <div style="text-align: center;">
     <img src="https://github.com/user-attachments/assets/76272400-9440-47ba-b662-b351304c952c" alt="track_object_animated" width="300"/>
@@ -39,7 +39,7 @@ This repository contains a Python script for real-time object tracking using YOL
 - **User-friendly interface** for selecting objects in video frames.
 - **Real-time display** of object coordinates.
 - **360° video support** for enhanced video experiences.
-<img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="900">
+  <img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="900">
 
 # 📖 Requirements
 
@@ -47,17 +47,23 @@ This repository contains a Python script for real-time object tracking using YOL
     gdown==5.2.0
     ipdb==0.13.13
     motmetrics==1.4.0
-    numpy==1.24.4
+    numpy==1.26.4
     opencv_python==4.8.1.78
     pandas==1.5.3
     PyYAML==6.0.1
     scipy==1.11.4
     torch==2.3.0
     torchvision==0.18.0
-    ultralytics
+    torchreid==0.2.5
+    scikit-learn==1.5.2
+    scikit-image==0.24.0
+    ultralytics==8.3.15
+    tensorboard==2.18.0
+
 <img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="900">
 
 # 📦 Installation
+
 ## 🚀 Ready, Set, Install! Let’s Get This Party Started! 🎉
 
 1. Clone this repository to your local machine:
@@ -65,15 +71,18 @@ This repository contains a Python script for real-time object tracking using YOL
    ```
    git clone https://github.com/MAVERICK-VF142/Object_tracking_in_360_video.git
    ```
+
 2. Install the required dependencies:
+
    ```
    pip install -r requirements.txt
    ```
 
 3. Download the YOLOv10 model weights (`yolov10e.pt`) and place them in the root directory of this repository.
-<img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="900">
+   <img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="900">
 
 # ▶️ Usage
+
 ## 💡 Let’s Dive Into the Magic! Here’s How to Use This Code! ✨
 
 1. Run the script `app.py`:
@@ -81,60 +90,65 @@ This repository contains a Python script for real-time object tracking using YOL
    ```
    python app.py
    ```
+
 2. Select an object in the video frame by clicking on it. The script will track the selected object and display its coordinates in real-time.
 
 3. Press 'q' to quit the application.
-<img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="900">
+   <img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="900">
 
 # 🛠️ Contributing
+
 ## 🤝 Your Ideas Matter! Help Us Shape the Future of This Project!
-
-
 
 Contributions are welcome! Here's how you can contribute:
 
 1. Fork the repository to your GitHub account.
 2. Clone the forked repository to your local machine:
-   
+
    ```
    git clone https://github.com/your-username/Object_tracking_in_360_video.git
    ```
+
 3. Create a new branch for your feature or bug fix:
-   
+
    ```
    git checkout -b feature-name
    ```
+
    Replace `feature-name` with a descriptive name for your feature or bug fix.
+
 4. Make your changes and commit them:
-   
+
    ```
    git add .
    git commit -m "Description of your changes"
    ```
+
 5. Push your changes to your forked repository:
-   
+
    ```
    git push origin feature-name
    ```
+
 6. Create a pull request from your forked repository to the main repository's `master` branch.
    **Note:** Please ensure your pull request adheres to the repository's contribution guidelines.
-Thank you for contributing to this project!
+   Thank you for contributing to this project!
 
 <img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="900">
 
 # 📜 License
 
 ## 🔑 Unlocking the Code: Understanding Our Licensing! 🔓
+
 This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
 
 <img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="900">
 
 # ❤️ Contributors
+
 ## 🌟 A Big Shoutout to Our Rockstars! Your Contributions Make Us Shine! 🎉
 
 <img src="https://github.com/user-attachments/assets/c6618a17-176e-4bb6-9f51-b3bf235c3e46" alt="happy-dancing" width="200" />
-
-
 
 [![Contributors](https://contrib.rocks/image?repo=MAVERICK-VF142/Object_tracking_in_360_video)](https://github.com/MAVERICK-VF142/Object_tracking_in_360_video/graphs/contributors)
 
--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import cv2
 import numpy as np
 import torch
-import torchvision.transforms as transforms
 from PIL import Image
 from ultralytics import YOLO
-from deep_sort_pytorch.utils.parser import get_config
 from deep_sort_pytorch.deep_sort import DeepSort
 from collections import defaultdict
 from sklearn.preprocessing import normalize
@@ -17,9 +15,11 @@ from Dependencies import download_files
 import logging
 
 # Set up logging configuration
-logging.basicConfig(filename='boundary_tracking.log',
-                    level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename="boundary_tracking.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 
 # Call the function to download the required files
@@ -54,13 +54,28 @@ previous_frames = {
 centroid_history = defaultdict(lambda: defaultdict(list))
 
 # Initialize a single tracker for the entire original frame
-tracker = DeepSort(model_path='deep_sort_pytorch/deep_sort/deep/checkpoint/ckpt.t7', max_age=15)
+tracker = DeepSort(
+    model_path="deep_sort_pytorch/deep_sort/deep/checkpoint/ckpt.t7", max_age=15
+)
 
 # Define colors for paths and bounding boxes
-path_colors = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 255), (0, 0, 255)]  # Yellow, Cyan, Green, Magenta, Red
+path_colors = [
+    (0, 255, 255),
+    (255, 255, 0),
+    (0, 255, 0),
+    (255, 0, 255),
+    (0, 0, 255),
+]  # Yellow, Cyan, Green, Magenta, Red
 bbox_colors = [(255, 0, 255)]  # Magenta
 
-motion_detected = {"1": False, "2": False, "3": False, "4": False, "5": False, "6": False}
+motion_detected = {
+    "1": False,
+    "2": False,
+    "3": False,
+    "4": False,
+    "5": False,
+    "6": False,
+}
 
 # Open the video file
 video_path = "Dependencies/Test.mp4"
@@ -83,38 +98,46 @@ global_id_map = defaultdict(dict)
 reid_features = defaultdict(lambda: defaultdict(list))
 
 # Load the OSNet model using torchreid
-osnet_model = models.build_model(
-    name='osnet_x1_0',
-    num_classes=1,
-    pretrained=True
-).to(device)
+osnet_model = models.build_model(name="osnet_x1_0", num_classes=1, pretrained=True).to(
+    device
+)
 osnet_model.eval()
 
 # Define the transform for the image
-transform = reid_transforms.Compose([
-    reid_transforms.Resize((256, 128)),  # Resize to (height, width)
-    reid_transforms.ToTensor(),
-    reid_transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+transform = reid_transforms.Compose(
+    [
+        reid_transforms.Resize((256, 128)),  # Resize to (height, width)
+        reid_transforms.ToTensor(),
+        reid_transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+        ),
+    ]
+)
+
 
 def match_features(feature1, feature2, threshold=0.5):
     feature1 = normalize([feature1])[0]
     feature2 = normalize([feature2])[0]
-    distance = cdist([feature1], [feature2], metric='euclidean')[0][0]
+    distance = cdist([feature1], [feature2], metric="euclidean")[0][0]
     return distance < threshold
+
 
 def extract_color_histogram(image, bins=(8, 8, 8)):
     hist = cv2.calcHist([image], [0, 1, 2], None, bins, [0, 256, 0, 256, 0, 256])
     cv2.normalize(hist, hist)
     return hist.flatten()
 
+
 def extract_lbp_features(image, num_points=24, radius=8):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     lbp = local_binary_pattern(gray, num_points, radius, method="uniform")
-    (hist, _) = np.histogram(lbp.ravel(), bins=np.arange(0, num_points + 3), range=(0, num_points + 2))
+    (hist, _) = np.histogram(
+        lbp.ravel(), bins=np.arange(0, num_points + 3), range=(0, num_points + 2)
+    )
     hist = hist.astype("float")
-    hist /= (hist.sum() + 1e-6)
+    hist /= hist.sum() + 1e-6
     return hist
+
 
 def extract_hog_features(image):
     winSize = (64, 128)
@@ -125,9 +148,10 @@ def extract_hog_features(image):
     hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins)
     return hog.compute(image).flatten()
 
+
 def extract_features(frame, bbox):
     x1, y1, x2, y2 = bbox
-    cropped_img = frame[int(y1):int(y2), int(x1):int(x2)]
+    cropped_img = frame[int(y1) : int(y2), int(x1) : int(x2)]
     if cropped_img.size == 0:
         return None
     resized_img = cv2.resize(cropped_img, (128, 256))
@@ -145,16 +169,22 @@ def extract_features(frame, bbox):
     hog_features = extract_hog_features(resized_img)
 
     # Concatenate all features
-    combined_features = np.hstack([features_osnet, color_hist, lbp_features, hog_features])
+    combined_features = np.hstack(
+        [features_osnet, color_hist, lbp_features, hog_features]
+    )
 
     return combined_features
+
 
 def detect_saliency(frame, previous_frame):
     if previous_frame is None:
         return 0
-    previous_frame_resized = cv2.resize(previous_frame, (frame.shape[1], frame.shape[0]))
+    previous_frame_resized = cv2.resize(
+        previous_frame, (frame.shape[1], frame.shape[0])
+    )
     saliency = np.mean(np.abs(frame - previous_frame_resized))
     return saliency
+
 
 object_fov_map = defaultdict(set)  # Import defaultdict from collections
 
@@ -190,6 +220,7 @@ def transform_perspective(frame, perspective, fov_height_factor=2.0):
 
     return perspective_frame, fov_center, fov_width, fov_height
 
+
 # Define the virtual boundaries based on FOVs
 virtual_boundaries = [0, 960, 2880, 3840, 4880, 5760]
 boundary_threshold = 500  # Set a threshold to determine proximity to a boundary
@@ -204,13 +235,11 @@ def handle_boundary_crossing(global_id, x1, x2, frame_width, buffer_zone):
                 x1 = 0
             if x2 > frame_width:
                 x2 = frame_width
-            
+
             # Check if the bounding box is near the boundary
             if x1 <= boundary + buffer_zone and x2 >= boundary - buffer_zone:
                 return global_id
     return None
-
-
 
 
 def merge_bounding_boxes_across_boundaries(bboxes, frame_width):
@@ -231,13 +260,16 @@ def merge_bounding_boxes_across_boundaries(bboxes, frame_width):
 
 
 # Initialize dictionary to track objects crossing boundaries
-crossing_objects = defaultdict(lambda: {'left': None, 'right': None})
+crossing_objects = defaultdict(lambda: {"left": None, "right": None})
 
 # Initialize dictionary to track objects crossing boundaries
 crossing_objects = defaultdict(lambda: None)
 
+
 # Update the function that checks and assigns IDs
-def update_id_for_crossing_objects(global_id, track_id, x1, x2, frame_width, buffer_zone):
+def update_id_for_crossing_objects(
+    global_id, track_id, x1, x2, frame_width, buffer_zone
+):
     """Update the ID for objects crossing boundaries based on their coordinates."""
     for boundary in virtual_boundaries:
         if is_near_boundary(x1, x2, boundary, buffer_zone):
@@ -249,45 +281,46 @@ def update_id_for_crossing_objects(global_id, track_id, x1, x2, frame_width, buf
             return track_id
     return None
 
-buffer_zone=20
+
+buffer_zone = 20
 
 # Initialize dictionary to track objects crossing boundaries with their history
-object_history = defaultdict(lambda: {'bbox': None, 'frames': 0})
+object_history = defaultdict(lambda: {"bbox": None, "frames": 0})
 
-def update_id_for_crossing_objects(global_id, track_id, x1, x2, frame_width, buffer_zone):
-    """Update the ID for objects crossing boundaries based on their coordinates."""
-    for boundary in virtual_boundaries:
-        if is_near_boundary(x1, x2, boundary, buffer_zone):
-            # If crossing the boundary, assign the same ID
-            return global_id
-    return None
 
 def is_near_boundary(x1, x2, boundary, buffer_zone):
     """Check if the bounding box is near the given boundary within the buffer zone."""
-    return (x1 <= boundary + buffer_zone and x2 >= boundary - buffer_zone)
+    return x1 <= boundary + buffer_zone and x2 >= boundary - buffer_zone
+
 
 from collections import defaultdict
 
 # Initialize dictionary to track objects near boundaries and their last known IDs
-near_boundary_objects = defaultdict(lambda: {'last_seen': None, 'last_id': None})
+near_boundary_objects = defaultdict(lambda: {"last_seen": None, "last_id": None})
+
 
 def update_near_boundary_objects(track_id, x1, x2, frame_width, buffer_zone):
     """Update objects near boundaries and their last known IDs."""
     for boundary in virtual_boundaries:
         if is_near_boundary(x1, x2, boundary, buffer_zone):
-            near_boundary_objects[track_id] = {'last_seen': boundary, 'last_id': global_id_map['all'].get(track_id)}
+            near_boundary_objects[track_id] = {
+                "last_seen": boundary,
+                "last_id": global_id_map["all"].get(track_id),
+            }
             return
     # If the object is not near a boundary, remove it from the dictionary
     if track_id in near_boundary_objects:
         del near_boundary_objects[track_id]
+
 
 def check_reappeared_object(track_id, x1, x2, frame_width, buffer_zone):
     """Check if an object reappears near a boundary and reassign the same ID if necessary."""
     for boundary in virtual_boundaries:
         if is_near_boundary(x1, x2, boundary, buffer_zone):
             if track_id in near_boundary_objects:
-                return near_boundary_objects[track_id]['last_id']
+                return near_boundary_objects[track_id]["last_id"]
     return None
+
 
 # Inside your main loop where you process tracks:
 while cap.isOpened():
@@ -299,7 +332,9 @@ while cap.isOpened():
 
     # Perform detection and transformation for each FOV
     for perspective in previous_frames.keys():
-        perspective_frame, fov_center, fov_width, fov_height = transform_perspective(frame, perspective, fov_height_factor=2.0)
+        perspective_frame, fov_center, fov_width, fov_height = transform_perspective(
+            frame, perspective, fov_height_factor=2.0
+        )
 
         # Perform detection on each perspective
         results = model(perspective_frame, classes=[0, 2, 3], device=device)[0]
@@ -317,17 +352,25 @@ while cap.isOpened():
                     orig_y2 = int(orig_y2) + fov_center["y"] - fov_height // 2
 
                     # Check if bounding box is within the frame bounds
-                    if (orig_x1 < 0 or orig_x1 >= frame_width or
-                        orig_y1 < 0 or orig_y1 >= frame_height or
-                        orig_x2 < 0 or orig_x2 >= frame_width or
-                        orig_y2 < 0 or orig_y2 >= frame_height):
+                    if (
+                        orig_x1 < 0
+                        or orig_x1 >= frame_width
+                        or orig_y1 < 0
+                        or orig_y1 >= frame_height
+                        or orig_x2 < 0
+                        or orig_x2 >= frame_width
+                        or orig_y2 < 0
+                        or orig_y2 >= frame_height
+                    ):
                         continue
 
-                    fov_detections[perspective].append({
-                        "bbox": (orig_x1, orig_y1, orig_x2, orig_y2),
-                        "conf": conf,
-                        "cls": cls,
-                    })
+                    fov_detections[perspective].append(
+                        {
+                            "bbox": (orig_x1, orig_y1, orig_x2, orig_y2),
+                            "conf": conf,
+                            "cls": cls,
+                        }
+                    )
 
     bbox_xywh = []
     confs = []
@@ -374,7 +417,9 @@ while cap.isOpened():
 
         # Draw bounding box and ID in each FOV
         for perspective in previous_frames.keys():
-            perspective_frame, fov_center, fov_width, fov_height = transform_perspective(frame, perspective, fov_height_factor=2.0)
+            perspective_frame, fov_center, fov_width, fov_height = (
+                transform_perspective(frame, perspective, fov_height_factor=2.0)
+            )
 
             # Adjust the coordinates to map onto the perspective frame
             perspective_x1 = x1 - (fov_center["x"] - fov_width // 2)
@@ -383,18 +428,32 @@ while cap.isOpened():
             perspective_y2 = y2 - (fov_center["y"] - fov_height // 2)
 
             # Check if the bounding box falls within this FOV
-            if (0 <= perspective_x1 < perspective_frame.shape[1]) and (0 <= perspective_y1 < perspective_frame.shape[0]):
-                cv2.rectangle(perspective_frame, (int(perspective_x1), int(perspective_y1)),
-                            (int(perspective_x2), int(perspective_y2)), bbox_colors[0], 2)
-                cv2.putText(perspective_frame, f"ID: {global_id}",
-                            (int(perspective_x1) + 10, int(perspective_y1) - 5),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+            if (0 <= perspective_x1 < perspective_frame.shape[1]) and (
+                0 <= perspective_y1 < perspective_frame.shape[0]
+            ):
+                cv2.rectangle(
+                    perspective_frame,
+                    (int(perspective_x1), int(perspective_y1)),
+                    (int(perspective_x2), int(perspective_y2)),
+                    bbox_colors[0],
+                    2,
+                )
+                cv2.putText(
+                    perspective_frame,
+                    f"ID: {global_id}",
+                    (int(perspective_x1) + 10, int(perspective_y1) - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 0, 0),
+                    2,
+                    cv2.LINE_AA,
+                )
                 object_fov_map[global_id].add(perspective)
 
             # Display the FOV frame
             cv2.imshow(f"FOV {perspective}", perspective_frame)
             frame_resized = cv2.resize(frame, (1500, 780))
-            cv2.imshow(f"obj",frame_resized)
+            cv2.imshow(f"obj", frame_resized)
 
     # Quit the loop if 'q' is pressed
     key = cv2.waitKey(1) & 0xFF
